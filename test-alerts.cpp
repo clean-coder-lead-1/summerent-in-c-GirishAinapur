97464c5a67b5e40a536f0a8ef1557a744dcefa4d
GirishAinapur <102650170+GirishAinapur@users.noreply.github.com>
2022-03-31 16:13:31 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -33,112 +33,50 @@ TEST_CASE("Colling Type High")
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30) == NORMAL);
 }
 
-
-TEST_CASE("Alert - Controller 1") 
+TEST_CASE("Passive Cooling")
 {
-  BatteryCharacter BatChar;
+    BatteryCharacter BattChar;
   
-  BatChar.coolingType = PASSIVE_COOLING;
-  BatChar.brand[48] = {0};
+    BattChar.coolingType = PASSIVE_COOLING;
   
-  checkAndAlert(TO_CONTROLLER, BatChar, -10);
+    checkAndAlert(TO_EMAIL, BattChar, 45);
+    checkAndAlert(TO_EMAIL, BattChar, -10);
+    checkAndAlert(TO_EMAIL, BattChar, 25);
+  
+    checkAndAlert(TO_CONTROLLER, BattChar, 25);
+    checkAndAlert(TO_CONTROLLER, BattChar, -5);
+    checkAndAlert(TO_CONTROLLER, BattChar, 45);
 }
 
-TEST_CASE("Alert - Controller 2") 
+TEST_CASE("Active Cooling")
 {
-  BatteryCharacter BatChar;
+    BatteryCharacter BattChar;
   
-  BatChar.coolingType = (CoolingType)4;
-  BatChar.brand[48] = {0};
+    BattChar.coolingType = HI_ACTIVE_COOLING;
   
-  checkAndAlert((AlertTarget)5, BatChar, 10);
+    checkAndAlert(TO_CONTROLLER, BattChar, 29);
+    checkAndAlert(TO_CONTROLLER, BattChar, -7);
+    checkAndAlert(TO_CONTROLLER, BattChar, 52);
+    
+    checkAndAlert(TO_EMAIL, BattChar, 51);
+    checkAndAlert(TO_EMAIL, BattChar, -8);
+    checkAndAlert(TO_EMAIL, BattChar, 25);
 }
 
-TEST_CASE("Alert - Controller 3") 
+TEST_CASE("Medium Active Cooling")
 {
-  BatteryCharacter BatChar;
+    BatteryCharacter BattChar;
   
-  BatChar.coolingType = PASSIVE_COOLING;
-  BatChar.brand[48] = {0};
+    BattChar.coolingType = MED_ACTIVE_COOLING;
   
-  checkAndAlert(TO_CONTROLLER, BatChar, -5);
+    checkAndAlert(TO_EMAIL, BattChar, 50);
+    checkAndAlert(TO_EMAIL, BattChar, -9);
+    checkAndAlert(TO_EMAIL, BattChar, 25);
+  
+    checkAndAlert(TO_CONTROLLER, BattChar, 27);
+    checkAndAlert(TO_CONTROLLER, BattChar, -3);
+    checkAndAlert(TO_CONTROLLER, BattChar, 55);
+    
+    
 }
 
-TEST_CASE("Alert - Controller 4") 
-{
-  BatteryCharacter BatChar;
-  
-  BatChar.coolingType = PASSIVE_COOLING;
-  BatChar.brand[48] = {0};
-  
-  checkAndAlert(TO_CONTROLLER, BatChar, 50);
-}
-
-
-TEST_CASE("Alert - Controller 5") 
-{
-  BatteryCharacter BatChar;
-  
-  BatChar.coolingType = PASSIVE_COOLING;
-  BatChar.brand[48] = {0};
-  
-  checkAndAlert(TO_CONTROLLER, BatChar, 0);
-}
-
-TEST_CASE("Alert - Email 1") 
-{
-  BatteryCharacter BatChar;
-  
-  BatChar.coolingType = MED_ACTIVE_COOLING;
-  BatChar.brand[48] = {0};
-  
-  checkAndAlert(TO_EMAIL, BatChar, 30);
-}
-
-TEST_CASE("Alert - Email 2") 
-{
-  BatteryCharacter BatChar;
-  
-  BatChar.coolingType = MED_ACTIVE_COOLING;
-  BatChar.brand[48] = {0};
-  
-  checkAndAlert(TO_EMAIL, BatChar, 30);
-}
-
-TEST_CASE("Alert - Email 3") 
-{
-  BatteryCharacter BatChar;
-  
-  BatChar.coolingType = HI_ACTIVE_COOLING;
-  BatChar.brand[48] = {0};
-  
-  checkAndAlert(TO_EMAIL, BatChar, 50);
-}
-
-
-TEST_CASE("Checks the temperature state of the battery and alerts accordingly")
-{
-    BatteryCharacter batteryChar;
-    checkAndAlert(TO_CONTROLLER, batteryChar, 25);
-    checkAndAlert(TO_CONTROLLER, batteryChar, 45);
-    checkAndAlert(TO_CONTROLLER, batteryChar, -2);
-    checkAndAlert(TO_EMAIL, batteryChar, 25);
-    checkAndAlert(TO_EMAIL, batteryChar, 45);
-    checkAndAlert(TO_EMAIL, batteryChar, -2);
-
-    batteryChar.coolingType = HI_ACTIVE_COOLING;
-    checkAndAlert(TO_CONTROLLER, batteryChar, 35);
-    checkAndAlert(TO_CONTROLLER, batteryChar, 50);
-    checkAndAlert(TO_CONTROLLER, batteryChar, -3);
-    checkAndAlert(TO_EMAIL, batteryChar, 35);
-    checkAndAlert(TO_EMAIL, batteryChar, 50);
-    checkAndAlert(TO_EMAIL, batteryChar, -3);
-
-    batteryChar.coolingType = MED_ACTIVE_COOLING;
-    checkAndAlert(TO_CONTROLLER, batteryChar, 30);
-    checkAndAlert(TO_CONTROLLER, batteryChar, 45);
-    checkAndAlert(TO_CONTROLLER, batteryChar, -1);
-    checkAndAlert(TO_EMAIL, batteryChar, 30);
-    checkAndAlert(TO_EMAIL, batteryChar, 45);
-    checkAndAlert(TO_EMAIL, batteryChar, -1);
-}
