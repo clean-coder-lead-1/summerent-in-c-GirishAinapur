10479e259328d4db3bd43793f205a73b06f35a8e
GirishAinapur <102650170+GirishAinapur@users.noreply.github.com>
2022-03-31 15:51:10 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,37 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
+TEST_CASE("infers the breach according to limits - Normal") 
+{
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
+
+TEST_CASE("infers the breach according to limits - Low") 
+{
+  REQUIRE(inferBreach(-10, 0, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach according to limits - High") 
+{
+  REQUIRE(inferBreach(50, 10, 30) == TOO_HIGH);
+}
+
+TEST_CASE("Colling Type Normal") 
+{
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
+}
+
+TEST_CASE("Colling Type Low") 
+{
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+}
+
+TEST_CASE("Colling Type High") 
+{
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 30) == NORMAL);
+}
+
+
 TEST_CASE("Alert - Controller 1") 
 {
   BatteryCharacter BatChar;
@@ -12,7 +43,6 @@ TEST_CASE("Alert - Controller 1")
   
   checkAndAlert(TO_CONTROLLER, BatChar, -10);
 }
-
 
 TEST_CASE("Alert - Controller 2") 
 {
@@ -45,7 +75,7 @@ TEST_CASE("Alert - Controller 4")
 }
 
 
-TEST_CASE("Alert - Controller 3") 
+TEST_CASE("Alert - Controller 5") 
 {
   BatteryCharacter BatChar;
   
@@ -65,7 +95,7 @@ TEST_CASE("Alert - Email 1")
   checkAndAlert(TO_EMAIL, BatChar, 30);
 }
 
-TEST_CASE("Alert - Email 3") 
+TEST_CASE("Alert - Email 2") 
 {
   BatteryCharacter BatChar;
   
@@ -75,7 +105,7 @@ TEST_CASE("Alert - Email 3")
   checkAndAlert(TO_EMAIL, BatChar, 30);
 }
 
-TEST_CASE("Alert - Email 2") 
+TEST_CASE("Alert - Email 3") 
 {
   BatteryCharacter BatChar;
   
